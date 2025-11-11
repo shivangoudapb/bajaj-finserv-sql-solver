fa4dd8ff217ae3d2519acdc53a820fa791d8867e
Shivangouda <bhavihalshivu@gmail.com>
2025-11-11 16:58:56 +0530

    Add files via upload

---
+++ src/main/java/com/bajajfinserv/SqlSolverApplication.java
@@ -0,0 +1,19 @@
+package com.bajajfinserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SqlSolverApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SqlSolverApplication.class, args);
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
